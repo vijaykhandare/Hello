70d2507ee54ebc39a134629bf5f6b92dbdfc295d
vijay <khandarevijay12@gmail.com>
2018-11-26 17:44:18 +0530

    for loop

--- simple/src/simple/Hello.java
+++ simple/src/simple/Hello.java
@@ -1,11 +1,22 @@
 package simple;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+
+
 public class Hello {
 
 	public static void main(String[] args) {
 
-System.out.println("hell");
-System.out.println("comm");
+		String []s={"vijay","sanjay"};
+		System.out.println("hello");
+		
+		for(String s1:s)
+		{
+			System.out.println(s1);
+		}
 	}
 
 }
